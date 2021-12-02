9d2f44f9cb2bdb478735be25bba9c5d444a484be
Kaan Hakan <kaanhakan2005@gmail.com>
2021-12-02 06:43:21 +0300

    chore: changed  some error handling logic

--- validate.go
+++ validate.go
@@ -31,7 +31,7 @@ func init() {
 
 }
 
-func (d document) insertBarcode() {
+func (d document) InsertBarcode() {
 	requestBody, err := json.Marshal(map[string]string{
 		"sorgulananBarkod": d.Barcode,
 		"token":            d.Token,
@@ -41,11 +41,15 @@ func (d document) insertBarcode() {
 		os.Exit(1)
 	}
 
-	body := makeRequest(http.MethodPost, "/belge-dogrulama?submit", bytes.NewBuffer(requestBody))
+	body, err := makeRequest(http.MethodPost, "/belge-dogrulama?submit", bytes.NewBuffer(requestBody))
+	if err != nil {
+		ErrorLogger.Println(err.Error())
+		os.Exit(1)
+	}
 	d.Token = extractToken(body)
 }
 
-func (d document) insertID() {
+func (d document) InsertID() {
 	requestBody, err := json.Marshal(map[string]string{
 		"ikinciAlan": d.ID,
 		"token":      d.Token,
@@ -55,13 +59,21 @@ func (d document) insertID() {
 		os.Exit(1)
 	}
 
-	body := makeRequest(http.MethodPost, "/belge-dogrulama?islem=dogrulama&submit", bytes.NewBuffer(requestBody))
+	body, err := makeRequest(http.MethodPost, "/belge-dogrulama?islem=dogrulama&submit", bytes.NewBuffer(requestBody))
+	if err != nil {
+		ErrorLogger.Println(err.Error())
+		os.Exit(1)
+	}
 	d.Token = extractToken(body)
 }
 
 // getToken gets the first token from the e-devlet website
-func (d document) getToken() {
-	body := makeRequest(http.MethodGet, "/belge-dogrulama", nil)
+func (d document) GetToken() {
+	body, err := makeRequest(http.MethodGet, "/belge-dogrulama", nil)
+	if err != nil {
+		ErrorLogger.Println(err.Error())
+		os.Exit(1)
+	}
 	token := extractToken(body)
 	d.Token = token
 }
@@ -74,12 +86,11 @@ func extractToken(body string) string {
 }
 
 // makeRequest makes a request to the given url and returns the response body
-func makeRequest(method string, path string, body io.Reader) string {
+func makeRequest(method string, path string, body io.Reader) (string, error) {
 	req, err := http.NewRequest(method, fmt.Sprintf("%s%s", eDevletURL, path), body)
 
 	if err != nil {
-		ErrorLogger.Println(err.Error())
-		os.Exit(1)
+		return "", err
 	}
 
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36")
@@ -87,8 +98,7 @@ func makeRequest(method string, path string, body io.Reader) string {
 	resp, err := client.Do(req)
 
 	if err != nil {
-		ErrorLogger.Println(err.Error())
-		os.Exit(1)
+		return "", err
 	}
 
 	defer resp.Body.Close()
@@ -96,9 +106,8 @@ func makeRequest(method string, path string, body io.Reader) string {
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
-		ErrorLogger.Println(err.Error())
-		os.Exit(1)
+		return "", err
 	}
 
-	return string(bodyBytes)
+	return string(bodyBytes), nil
 }
